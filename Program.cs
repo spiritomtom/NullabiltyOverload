08393852cba48986a0b4ca66a5a4ff623ab54f44
Petko Dapchev <Petko.Dapchev@docuware.com>
2022-10-05 15:35:36 +0300

    Commit eproducing sample

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using NullabilityTest;
+using System;
+
+namespace NullabillityTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var tc = new TestClass();
+            var guidArray = (Guid[]?)new Guid[3];
+
+            tc += guidArray;
+
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullabilityTest
+{
+    internal class TestClass
+    {
+        public static TestClass operator+ (TestClass x, Guid[]? y)
+        {
+            return new TestClass();
+        }
+    }
+}
